28ca8e7f9461b265d78790d00cd834a16b0ce900
steph javier <stephinijavi@gmail.com>
2022-08-14 11:50:25 -0700

    update

--- main.js
+++ main.js
@@ -1,134 +1,125 @@
-const servers = {'iceServers': [{'url': 'stun:stun.l.google.com:19302'}]};
+const servers = { iceServers: [{ url: "stun:stun.l.google.com:19302" }] };
 
 let peerConnection = new RTCPeerConnection(servers);
 
-const mediaStreamConstraints = {video: true, audio: true};
-const offerOptions = {offerToReceiveVideo:1}; //boolean for 1= true? 0=false?
+const mediaStreamConstraints = { video: true, audio: true };
+const offerOptions = { offerToReceiveVideo: 1 }; //boolean for 1= true? 0=false?
 
 const init = async () => {
-	peerConnection;
-	console.log('Created peer connection');
+  peerConnection;
+  console.log("Created peer connection");
 
-//local video
-	const peerMediaStream = new MediaStream();
-			peerConnection.addStream(peerMediaStream);
+  //local video
+  const peerMediaStream = new MediaStream();
+  peerConnection.addStream(peerMediaStream);
 
-			navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
-				.then((mediaStream) => {
-				
-					localVideo.srcObject = mediaStream;
+  navigator.mediaDevices
+    .getUserMedia(mediaStreamConstraints)
+    .then((mediaStream) => {
+      localVideo.srcObject = mediaStream;
 
-					for (const track of mediaStream.getTracks())
-						peerMediaStream.addTrack(track);
-					}
-				)
-				.catch(logError);
+      for (const track of mediaStream.getTracks())
+        peerMediaStream.addTrack(track);
+    })
+    .catch(logError);
+};
+
+//elements from HTML
+const localVideo = document.getElementById("localVideo");
+const remoteVideo = document.getElementById("remoteVideo");
+
+const localOffer = document.getElementById("localOffer");
+const remoteOffer = document.getElementById("remoteOffer");
+const createOffer = document.getElementById("createOffer");
+const acceptOffer = document.getElementById("acceptOffer");
+
+const localAnswer = document.getElementById("localAnswer");
+const remoteAnswer = document.getElementById("remoteAnswer");
+const acceptAnswer = document.getElementById("acceptAnswer");
+
+//	assign click handlers to each button
+createOffer.addEventListener("click", offerCreate);
+acceptOffer.addEventListener("click", offerAccept);
+acceptAnswer.addEventListener("click", answerAccept);
+
+peerConnection.addEventListener("icecandidate", handleConnection);
+peerConnection.addEventListener("addstream", gotRemoteMediaStream);
+
+function handleConnection(event) {
+  const connection = event.target;
+  const iceCandidate = event.candidate;
+
+  if (iceCandidate == null) {
+    const description = connection.localDescription;
+    const descriptionType = description.type;
+    const descriptionString = JSON.stringify(description);
+
+    if (descriptionType == "offer") {
+      localOffer.value = descriptionString;
+    } else {
+      localAnswer.value = descriptionString;
+    }
+  }
 }
-	
-			//elements from HTML
-			const localVideo = document.getElementById('localVideo');
-			const remoteVideo = document.getElementById('remoteVideo');
 
-			const localOffer = document.getElementById('localOffer');
-			const remoteOffer = document.getElementById('remoteOffer');
-			const createOffer = document.getElementById('createOffer');
-			const acceptOffer = document.getElementById('acceptOffer');
+function gotRemoteMediaStream(event) {
+  const mediaStream = event.stream;
+  remoteVideo.srcObject = mediaStream;
+  console.log("remote peer connection received remote stream");
+}
 
-			const localAnswer = document.getElementById('localAnswer');
-			const remoteAnswer = document.getElementById('remoteAnswer');
-			const acceptAnswer = document.getElementById('acceptAnswer');
+function offerCreate() {
+  peerConnection.createOffer(offerOptions).then(createdOffer).catch(logError);
+}
 
-			//	assign click handlers to each button
-			createOffer.addEventListener('click', offerCreate);
-			acceptOffer.addEventListener('click', offerAccept);
-			acceptAnswer.addEventListener('click', answerAccept);
-					
-			peerConnection.addEventListener('icecandidate', handleConnection);
-			peerConnection.addEventListener('addstream', gotRemoteMediaStream);
-				
-			function handleConnection(event) {
+function createdOffer(description) {
+  peerConnection
+    .setLocalDescription(description)
+    .then(() => {
+      console.log("local peer description set");
+    })
+    .catch(logError);
+}
 
-				const connection = event.target;
-				const iceCandidate = event.candidate;
-				
-				if (iceCandidate == null)
-				{
-					const description = connection.localDescription;
-					const descriptionType = description.type;
-					const descriptionString = JSON.stringify(description);
-					
-					if (descriptionType == 'offer') {
+function offerAccept() {
+  const description = JSON.parse(remoteOffer.value);
 
-						localOffer.value = descriptionString;
-						
-					} else {
-						
-						localAnswer.value = descriptionString;
-					}
-				}
-			}
-					
-			function gotRemoteMediaStream(event) {
+  peerConnection
+    .setRemoteDescription(description)
+    .then(() => {
+      console.log("remote peer offer accepted");
+    })
+    .catch(logError);
 
-				const mediaStream = event.stream;
-				remoteVideo.srcObject = mediaStream;
-				console.log('remote peer connection received remote stream')
-			}
+  peerConnection.createAnswer().then(createdAnswer).catch(logError);
+}
 
-			function offerCreate() {
+function createdAnswer(description) {
+  peerConnection
+    .setLocalDescription(description)
+    .then(() => {
+      console.log("remote peer answered");
+    })
+    .catch(logError);
+}
 
-				peerConnection.createOffer(offerOptions)
-					.then(createdOffer)
-					.catch(logError);
-			}
+function answerAccept() {
+  const description = JSON.parse(remoteAnswer.value);
 
-			function createdOffer(description) {
+  console.log(description);
 
-				peerConnection.setLocalDescription(description)
-					.then(() => {console.log('local peer description set');})
-					.catch(logError);
-			}
+  peerConnection
+    .setRemoteDescription(description)
+    .then(() => {
+      console.log("Local peer remote description set");
+    })
+    .catch(logError);
+}
 
+function logError(error) {
+  console.log(error.toString());
+}
 
-			function offerAccept() {
-
-				const description = JSON.parse(remoteOffer.value);
-				
-				peerConnection.setRemoteDescription(description)
-					.then(() => {console.log('remote peer offer accepted');})
-					.catch(logError);
-					
-				peerConnection.createAnswer()
-					.then(createdAnswer)
-					.catch(logError);
-				
-			}
-
-			function createdAnswer(description) {
-
-				peerConnection.setLocalDescription(description)
-					.then(() => {console.log('remote peer answered');})
-					.catch(logError);
-			}
-
-			function answerAccept() {
-
-				const description = JSON.parse(remoteAnswer.value);
-
-				console.log(description)
-
-				peerConnection.setRemoteDescription(description)
-					.then(() => {console.log('Local peer remote description set');})
-					.catch(logError);
-				
-			}
-					
-					
-			function logError(error) {
-
-				console.log(error.toString());
-			}
-		
 //====================Screen Share===============
 const localScreen = document.getElementById("localScreen");
 const remoteScreen = document.getElementById("remoteScreen");
@@ -149,44 +140,75 @@ const stopRemoteScreen = document.getElementById("stopRemoteScreen");
 //   return captureStream;
 //   }
 
-  async function startCapture(displayMediaOptions) {
-    try {
-      localScreen.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
-    } catch (err) {
-      console.error(`Error: ${err}`);
-    }
+async function startCapture(displayMediaOptions) {
+  try {
+    localScreen.srcObject = await navigator.mediaDevices.getDisplayMedia(
+      displayMediaOptions
+    );
+  } catch (err) {
+    console.error(`Error: ${err}`);
   }
+}
 
-  function stopCapture(e) {
-    let tracks = localScreen.srcObject.getTracks();
-    tracks.forEach((track) => track.stop());
-    localScreen.srcObject = null;
+async function startRemoteCapture(displayMediaOptions) {
+  try {
+    remoteScreen.srcObject = await navigator.mediaDevices.getDisplayMedia(
+      displayMediaOptions
+    );
+  } catch (err) {
+    console.error(`Error: ${err}`);
   }
+}
 
-  //plays screen share media
-  // const setLocalStream = (captureStream) => {
-  //   localScreen.srcObject = captureStream
-  //   localScreen.muted = true;
-  //   localScreen.onplay()
-  // }
+function stopRemoteCapture() {
+  let tracks = remoteScreen.srcObject.getTracks();
+  tracks.forEach((track) => track.stop());
+  localScreen.srcObject = null;
+}
 
+function stopCapture() {
+  let tracks = localScreen.srcObject.getTracks();
+  tracks.forEach((track) => track.stop());
+  localScreen.srcObject = null;
+}
 
-startLocalScreen.addEventListener("click", (e) => {
-  startCapture();
-}, false)
+//plays screen share media
+// const setLocalStream = (captureStream) => {
+//   localScreen.srcObject = captureStream
+//   localScreen.muted = true;
+//   localScreen.onplay()
+// }
 
-stopLocalScreen.addEventListener("click", (e) => {
-  stopCapture();
-}, false)
+startLocalScreen.addEventListener(
+  "click",
+  (e) => {
+    startCapture();
+  },
+  false
+);
 
-startRemoteScreen.addEventListener("click", (e) => {
-  startCapture();
-}, false)
+stopLocalScreen.addEventListener(
+  "click",
+  (e) => {
+    stopCapture();
+  },
+  false
+);
 
-stopRemoteScreen.addEventListener("click", (e) => {
-  stopCapture();
-}, false)
+startRemoteScreen.addEventListener(
+  "click",
+  (e) => {
+    startRemoteCapture();
+  },
+  false
+);
 
-init()
-		
-	
+stopRemoteScreen.addEventListener(
+  "click",
+  (e) => {
+    stopRemoteCapture();
+  },
+  false
+);
+
+init();
